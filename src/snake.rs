09e9bb23daa8cd2b17787e01ae77712eb9629baf
Jisook93 <leejisook0718@gmail.com>
2023-06-28 17:41:24 +0900

    [Style] Fix convention to 4 spaces

--- src/snake.rs
+++ src/snake.rs
@@ -3,52 +3,52 @@ use crate::point::Point;
 
 #[derive(Debug)]
 pub struct Snake {
-  body: Vec<point>,
-  direction: Direction,
-  digesting: bool,
+    body: Vec<point>,
+    direction: Direction,
+    digesting: bool,
 }
 
 impl Snake {
-  pub fn new(start: Point, length: u16, direction: Direction) -> Self{
-    let opposite = direction.opposite();
-    let body: Vec<Point> = (0..length)
-      .into_iter() //std::ops::Range::into_iter -> Creates an iterator from a value.
-      .map(|i| start.transform(opposite, i))
-      .collect();
+    pub fn new(start: Point, length: u16, direction: Direction) -> Self{
+        let opposite = direction.opposite();
+        let body: Vec<Point> = (0..length)
+            .into_iter() //std::ops::Range::into_iter -> Creates an iterator from a value.
+            .map(|i| start.transform(opposite, i))
+            .collect();
 
-    Self {body, direction, digesting: false}
-  }
-
-  pub fn get_head_point(&self) -> Point {
-    self.body.first().unwrap().clone() //body 벡터 값 중 첫번째 아이템만 가져와서 복사.
-  }
-
-  pub fn get_body_point(&self) -> Vec<Point> {
-    self.body.clone()
-  }
-
-  pub fn get_direction(&self) -> Direction {
-    self.direction.clone()
-  }
-
-  pub fn contains_point(&self, point: &Point) -> bool {
-    self.body.contains(point)
-  }
-
-  pub fn slither(&mut self) {
-    self.body.insert(0, self.body.first().unwrap().transform(self.direction, 1));
-    if !self.digesting {
-      self.body.remove(self.body.len() - 1);
-    } else {
-      self.digesting = false;
+        Self {body, direction, digesting: false}
     }
-  }
 
-  pub fn set_direction (&mut self, direction: Direction) {
-    self.direction = direction;
-  }
+    pub fn get_head_point(&self) -> Point {
+        self.body.first().unwrap().clone() //body 벡터 값 중 첫번째 아이템만 가져와서 복사.
+    }
 
-  pub fn grow(&mut self) {
-    self.digesting = true;
-  }
+    pub fn get_body_point(&self) -> Vec<Point> {
+        self.body.clone()
+    }
+
+    pub fn get_direction(&self) -> Direction {
+        self.direction.clone()
+    }
+
+    pub fn contains_point(&self, point: &Point) -> bool {
+        self.body.contains(point)
+    }
+
+    pub fn slither(&mut self) {
+        self.body.insert(0, self.body.first().unwrap().transform(self.direction, 1));
+        if !self.digesting {
+            self.body.remove(self.body.len() - 1);
+        } else {
+            self.digesting = false;
+        }
+    }
+
+    pub fn set_direction(&mut self, direction: Direction) {
+        self.direction = direction;
+    }
+
+    pub fn grow(&mut self) {
+        self.digesting = true;
+    }
 }
